055e7db89dfa9c700b794f8913e79ccf02bf9b93
IlyaEfremov2 <IlyaEfremov8542@yandex.ru>
2024-10-02 19:00:04 +0300

    Add files via upload
    загрузил решение 1.3

---
+++ 1.3.c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>  
+#include <math.h> 
+/*
+* @brief Считывает вещественное число.
+* @return Вещественное число.
+*/
+double input(void);
+/*
+* @brief Рассчитывает сопротивление сети
+* @param x - сопротивление первого резистора
+* @param y - сопротивление второго резистора
+* @param z - сопротивление третьего резистора
+* @return Сопротивление сети
+*/
+double get_a(const double x, const double y, const double z);
+/*
+* @brief вход в програsмму
+* @return 0 в случае успеха
+*/
+int main(void)
+{
+	printf("введите сопротивление первого резистора:\n");
+	double x = input();
+	printf("введите сопротивление второго резистора:\n");
+	double y = input();
+	printf("введите сопротивление третьего резистора:\n");
+	double z = input();
+	printf("Cопротивление соединения= %f\n", get_a(x, y, z));
+	return 0;
+}
+double input(void)
+{
+	double value = 0.0;
+	scanf("%lf", &value);
+	return value;
+}
+double get_a(const double x, const double y, const double z)
+{
+	return x + y + z;
+}
