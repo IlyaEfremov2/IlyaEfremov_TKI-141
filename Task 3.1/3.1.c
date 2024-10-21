a940cce716e2abd44126ea78c9fbae808acc0ea9
llyaEfremov2 <lyaEfremov8542@yandex.ru>
2024-10-21 17:43:14 +0300

    убрал пустые строки, добавил проверку по ООФ и убрал ненужную проверку

--- Task 3.1/3.1.c
+++ Task 3.1/3.1.c
@@ -1,26 +1,19 @@
-﻿
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <math.h>
 #include <locale.h>
 #include <errno.h>
 #include <stdlib.h> 
-/**
-*@brief Проверка на то, чтобы конец отрезка был меньше начала
-*@return Продолжает решение, если выполнено успешно, или ошибку, если иначе
-*/
-double examination(const double x_start, const double x_finish, const double x_step);
+#include <float.h>
+double examination(const double x_start);
 /**
 *@brief Проверка введенных значений.
 *@return Возвращает значение, если выполнено успешно, или ошибку, если иначе
-*/
-double input(void);
+*/double input(void);
 /**
-* @brief Считает значение в промежутке
 * @param x_start - начальное значение
 * @param x_finish - конечное значение
 * @return значение при опрееленном x_start
-*/
-double get_function(const double x_start, const double x_finish);
+*/double get_function(const double x_start);
 /**
 * @brief Точка входа в програsмму
 * @return 0 в случае успеха
@@ -34,17 +27,17 @@ int main(void)
 	double x_finish = input();
 	puts("Шаг:");
 	double x_step = input();
-	if ((examination(x_start, x_finish,x_step)) == 1)
-	{
-		perror("Конечное значение не может быть больше начального");
+	if (examination(x_start)==1)
+	{ 
+		perror("Не входит по ООФ");
 		exit(EXIT_FAILURE);
 	}
-	if ((examination(x_start, x_finish, x_step)) == 0)
-	{ 
-		while (x_start <= (x_finish))
+	else
+	{
+		while (x_start <= x_finish + DBL_EPSILON)
 		{
 			printf("при x равном %f", x_start);
-			printf(" решение %f\n", get_function(x_start, x_finish));
+			printf(" решение %f\n", get_function(x_start));
 			x_start = x_start + x_step;
 		}
 	}
@@ -62,18 +55,18 @@ double input(void)
 	}
 	return value;
 }
-double get_function(const double x_start, const double x_finish)
+double get_function(const double x_start)
 {
 	return (0.1 * pow(x_start, 2) - x_start*log(x_start)) ;
 }
-double examination(const double x_start, const double x_finish,const double x_step)
+double examination(const double x_start)
 {
-	if (x_finish>=x_start+x_step)
+	if (x_start <= 0)
 	{
-		return 0;
+		return 1;
 	}
 	else
 	{
-		return 1;
+		return 0;
 	}
 }
