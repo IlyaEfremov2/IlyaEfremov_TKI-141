df9c4df17f67fe3a2833d475569021f5fa8f52c8
IlyaEfremov2 <IlyaEfremov8542@yandex.ru>
2024-10-28 17:07:41 +0300

    Update 3.1.c
    исправил неисрпавности

--- Task 3.1/3.1.c
+++ Task 3.1/3.1.c
@@ -1,24 +1,31 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <math.h>
 #include <locale.h>
 #include <errno.h>
 #include <stdlib.h> 
 #include <float.h>
 #include <stdbool.h>
+
 /**
 *@brief Проверка введенных значений на вход по ОФФ
 * * @param x_start - начальное значение
 *@return Выводит ошибку, если не входит в ООФ, или ответ, если иначе
-*/bool examination(float x_start);
+*/
+bool examination(float x_start);
+
 /**
 *@brief Проверка введенных значений.
 *@return Возвращает значение, если выполнено успешно, или ошибку, если иначе
-*/double input(void);
+*/
+double input(void);
+
 /**
 * @param x_start - начальное значение
 * @param x_finish - конечное значение
 * @return значение при опрееленном x_start
-*/double get_function(const double x_start);
+*/
+double get_function(const double x_start);
+
 /**
 * @brief Точка входа в програsмму
 * @return 0 в случае успеха
@@ -34,18 +41,16 @@ int main(void)
 	double x_step = input();
 	while (x_start <= x_finish + DBL_EPSILON)
 	{
+		printf("при x равном %f", x_start);
 		if (examination(x_start))
 		{
-			printf("при x равном %f", x_start);
-			printf("решение %f\n", get_function(x_start));
-			x_start = x_start + x_step;
+			printf(" решение %f\n", get_function(x_start));
 		}
 		else
 		{
-			printf("при x равном %f", x_start);
 			printf("решение  отсутствует\n");
-			x_start = x_start + x_step;
 		}
+		x_start = x_start + x_step;
 	}
 	return 0;
 }
