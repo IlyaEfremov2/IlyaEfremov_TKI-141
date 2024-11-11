3c32cae8386aab731d90959eb13be15e7ca14ed0
IlyaEfremov2 <IlyaEfremov8542@yandex.ru>
2024-11-11 18:14:18 +0300

    Update Task 2.3.c
    убрал else

--- Task 2.3/Task 2.3.c
+++ Task 2.3/Task 2.3.c
@@ -19,39 +19,35 @@ bool check_minutes(double minutes);
 */
 int main()
 {
- puts("сколько часов\n");
- int hours = input();
-  puts("сколько минут\n");
- int minutes = input();
- if (!check_minutes(minutes))
-    {
-         return 1;
-    }
-else
-{
+    puts("сколько часов\n");
+    int hours = input();
+    puts("сколько минут\n");
+    int minutes = input();
+    if (!check_minutes(minutes))
+        {
+            return 1;
+        }
     if (((hours >= 0) && (hours < 6)))
-
-     {
-      printf("Доброй ночи");
-     }
-     else if (((hours >= 6) && (hours < 12)))
-     {
-      printf("Доброе утро");
-     }
-     else if (((hours >= 12) && (hours < 18)))
-     {
-      printf("Добрый день");
-     }
-     else if (((hours >= 18) && (hours < 24)))
-     {
-      printf("добрый вечер");
-     }
-     else 
-     {
+    {
+        printf("Доброй ночи");
+    }
+    else if (((hours >= 6) && (hours < 12)))
+    {
+        printf("Доброе утро");
+    }
+    else if (((hours >= 12) && (hours < 18)))
+    {
+        printf("Добрый день");
+    }
+    else if (((hours >= 18) && (hours < 24)))
+    {
+        printf("добрый вечер");
+    }
+    else 
+    {
         return 1;
-     }
+    }
     return 0;
- }
 }
 bool check_minutes(double minutes)
 {
